43f19e883f94d455a4d8fa186cb6bce69fc5af56
Dominik Bisiakowski <dombisiakowski@gmail.com>
2020-02-11 01:03:46 +0100

    addign generating table for choosen employer

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
 //fetching employer list
-async function getUserAsync() 
-{
-  let response = await fetch(`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers`);
-  let employers = await response.json();
-  return employers;
+async function getEmployersAsync() {
+    let response = await fetch(`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers`);
+    let employers = await response.json();
+    return employers;
 }
 
 //generating table with employers
-getUserAsync()
-  .then(employers => employers.map((emp) => {
-      const panel = document.createElement('div')
-      panel.classList.add('panel-info')
-    document.querySelector("aside").appendChild(panel);
-    panel.innerHTML = `
+getEmployersAsync()
+    .then(employers => employers.map((emp) => {
+        const panel = document.createElement('div')
+        panel.classList.add('panel-info')
+        document.querySelector('aside').appendChild(panel);
+        panel.innerHTML = `
     <div class="panel-heading">
-                    <h3 class="panel-title">${emp.name} ${emp.surename}</h3>
-                </div>
-                <div class="panel-body">
-                    <p>${emp.email}</p>
-                    <p>${emp.phone}</p>
-                    <button type="button" class="btn-info" onclick="getUserDetails(${emp.id})">Click to View Details</button>
-                </div>
+        <h3 class="panel-title">${emp.name} ${emp.surename}</h3>
+    </div>
+        <div class="panel-body">
+            <p>${emp.email}</p>
+            <p>${emp.phone}</p>
+            <button type="button" class="btn-info" onclick="getUserDetails(${emp.id})">Click to View Details</button>
+        </div>
     `
-   })); 
 
-//generating table for choosen employer
-
-
-   const  getUserDetails = (id) => {
-    console.log(id)
-    return "Hello World!";
-  }
+        //generating table for choosen employer
+        getUserDetails = (id) => {
+            document.querySelector('section').innerHTML = '';
+            const details = document.createElement('div')
+            details.classList.add('employer-details')
+            document.querySelector('section').appendChild(details);
+            details.innerHTML = `
+        <div class="panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">${employers[id].name} ${employers[id].surename}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>Name : ${employers[id].name}</p>
+                        <p>Surename: ${employers[id].surename}</p>
+                        <p>Pesel : ${employers[id].pesel}</p>
+                        <p>Email : ${employers[id].email}</p>
+                        <p>Phone : ${employers[id].phone}</p>
+                        <p>Team : ${employers[id].team}</p>
+                        <p>Job Profile : ${employers[id].jobProfile}</p>
+                    </div>
+        `
+        }
+    }))
