d0b160367b59f109e57b56c2fb56616ddf88f13b
Przemyslaw Pierzchalka <p.pierzchalka@vistex.com>
2020-02-14 10:33:21 +0100

    review

--- js/main.js
+++ js/main.js
@@ -1,151 +1,173 @@
 //fetching employer list
 async function getEmployersAsync() {
-    let response = await fetch(`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers`);
-    let employers = await response.json();
-    return employers;
+	let response = await fetch(
+		// TODO: use const;
+		`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers`
+	);
+	let employers = await response.json(); // TODO: use const;
+	return employers;
 }
 
 //generating table with employers
-getEmployersAsync()
-    .then(employers => employers.map((emp) => {
-        const panel = document.createElement('div')
-        panel.classList.add('panel-info')
-        document.querySelector('aside').appendChild(panel);
-        panel.innerHTML = `
-    <div class="panel-heading">
-        <h3 class="panel-title">${emp.name} ${emp.surename}</h3>
-    </div>
-        <div class="panel-body">
-            <p>${emp.email}</p>
-            <p>${emp.phone}</p>
-            <button type="button" class="btn-info" onclick="getUserDetails(${emp.id})">Click to View Details</button>
-        </div>
-    `
+getEmployersAsync().then(employers =>
+	employers.map(emp => {
+		const panel = document.createElement("div"); // TODO: generating panel for users list could be a separate function
+		panel.classList.add("panel-info");
+		document.querySelector("aside").appendChild(panel);
+		panel.innerHTML = `
+	<div class="panel-heading">
+		<h3 class="panel-title">${emp.name} ${emp.surename}</h3>
+	</div>
+		<div class="panel-body">
+			<p>${emp.email}</p>
+			<p>${emp.phone}</p>
+			<button type="button" class="btn-info" onclick="getUserDetails(${emp.id})">Click to View Details</button>
+		</div>
+	`;
 
-        //generating table for choosen employer
-        getUserDetails = (id) => {
-            document.querySelector('section').innerHTML = '';
-            const details = document.createElement('div');
-            details.classList.add('employer-details');
-            document.querySelector('section').appendChild(details);           
+		//generating table for choosen employer
+		getUserDetails = id => {
+			// TODO: employers[id] won't work for last user since it search by index of element in array
+			document.querySelector("section").innerHTML = "";
+			const details = document.createElement("div");
+			details.classList.add("employer-details");
+			document.querySelector("section").appendChild(details);
 
-            if (id === undefined) {
-                details.innerHTML = `
-                <div class="panel-info">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">new user</h3>
-                            </div>
-                            <div class="panel-body">
-                                <p>Name : <input class="employer-detail" id="name" autocomplete="off" onClick="wasClicked(this)"></p>
-                                <p>Surename: <input  class="employer-detail" id="surename" autocomplete="off" onClick="wasClicked(this)"></p>
-                                <p>Pesel : <input  class="employer-detail" id="pesel" autocomplete="off" onClick="wasClicked(this)"></p>
-                                <p>Email : <input  class="employer-detail" id="email" autocomplete="off" onClick="wasClicked(this)"></p>
-                                <p>Phone : <input  class="employer-detail" id="phone" autocomplete="off" onClick="wasClicked(this)"></p>
-                                <p>Team : <input  class="employer-detail" id="team" autocomplete="off" onClick="wasClicked(this)"></p>
-                                <p>Job Profile : <input  class="employer-detail" autocomplete="off" onClick="wasClicked(this)" id="profile"></p>
-                                <button type="button" class="btn-info" onclick="editUserDetails()">Add User</button>        
-                            </div>
-                `
-            }else{
-                details.innerHTML = `
-                <div class="panel-info">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">${employers[id].name} ${employers[id].surename}</h3>
-                            </div>
-                            <div class="panel-body">
-                                <p>Name : <input class="employer-detail" id="name" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].name}" ></p>
-                                <p>Surename: <input  class="employer-detail" id="surename" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].surename}"></p>
-                                <p>Pesel : <input  class="employer-detail" id="pesel" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].pesel}"></p>
-                                <p>Email : <input  class="employer-detail" id="email" autocomplete="off" onClick="wasClicked(this)"  value="${employers[id].email}"></p>
-                                <p>Phone : <input  class="employer-detail" id="phone" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].phone}"></p>
-                                <p>Team : <input  class="employer-detail" id="team" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].team}"></p>
-                                <p>Job Profile : <input  class="employer-detail" autocomplete="off" onClick="wasClicked(this)" id="profile" value="${employers[id].profile}"></p>
-                                <button type="button" class="btn-info" onclick="editUserDetails(${employers[id].id})">Click to Update</button>
-                                <button type="button" class="btn-remove" onclick="dismissalUser(${employers[id].id})">Click to Dismissal</button>
-        
-                            </div>
-                `
-            }
-        }
-    }))
+			// TODO: details.innerHTML could be stored as separate HTML template and do not need to be duplicated
+			// TODO: {
+			// TODO:  if (id) {
+			// TODO: 		 return createUserPanel(user);
+			// TODO:    }
+			// TODO:    return createUserPanel();
+			// TODO: }
+			// TODO:
+			if (id === undefined) {
+				details.innerHTML = `
+				<div class="panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title">new user</h3>
+							</div>
+							<div class="panel-body">
+								<p>Name : <input class="employer-detail" id="name" autocomplete="off" onClick="wasClicked(this)"></p>
+								<p>Surename: <input  class="employer-detail" id="surename" autocomplete="off" onClick="wasClicked(this)"></p>
+								<p>Pesel : <input  class="employer-detail" id="pesel" autocomplete="off" onClick="wasClicked(this)"></p>
+								<p>Email : <input  class="employer-detail" id="email" autocomplete="off" onClick="wasClicked(this)"></p>
+								<p>Phone : <input  class="employer-detail" id="phone" autocomplete="off" onClick="wasClicked(this)"></p>
+								<p>Team : <input  class="employer-detail" id="team" autocomplete="off" onClick="wasClicked(this)"></p>
+								<p>Job Profile : <input  class="employer-detail" autocomplete="off" onClick="wasClicked(this)" id="profile"></p>
+								<button type="button" class="btn-info" onclick="editUserDetails()">Add User</button>
+							</div>
+				`;
+			} else {
+				details.innerHTML = `
+				<div class="panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title">${employers[id].name} ${employers[id].surename}</h3>
+							</div>
+							<div class="panel-body">
+								<p>Name : <input class="employer-detail" id="name" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].name}" ></p>
+								<p>Surename: <input  class="employer-detail" id="surename" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].surename}"></p>
+								<p>Pesel : <input  class="employer-detail" id="pesel" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].pesel}"></p>
+								<p>Email : <input  class="employer-detail" id="email" autocomplete="off" onClick="wasClicked(this)"  value="${employers[id].email}"></p>
+								<p>Phone : <input  class="employer-detail" id="phone" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].phone}"></p>
+								<p>Team : <input  class="employer-detail" id="team" autocomplete="off" onClick="wasClicked(this)" value="${employers[id].team}"></p>
+								<p>Job Profile : <input  class="employer-detail" autocomplete="off" onClick="wasClicked(this)" id="profile" value="${employers[id].profile}"></p>
+								<button type="button" class="btn-info" onclick="editUserDetails(${employers[id].id})">Click to Update</button>
+								<button type="button" class="btn-remove" onclick="dismissalUser(${employers[id].id})">Click to Dismissal</button>
+							</div>
+				`;
+			}
+		};
+	})
+);
 
+//remove employer details section
+const showEmptyPanel = () => {
+	document.querySelector("section").innerHTML = ""; // TODO: do not select elements by html tags // this could be used to clear multiple elements
+};
 
-    
+//remove user
+const dismissalUser = id => {
+	fetch(
+		`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers/${id}`,
+		{
+			method: "DELETE"
+		}
+	).then(swal("successful", "user dismissaled", "success"));
+};
 
-    //remove employer details section
-    const showEmptyPanel = () => {
-        document.querySelector('section').innerHTML = '';
-        
-    }
+//add class for editUserDetails
+function wasClicked(e) {
+	e.classList.add("clicked");
+}
 
+//check which input was changed
+const editUserDetails = id => {
+	const clicked = document.getElementsByClassName("clicked");
+	let obj = {}; // TODO: use meaningful variable names
 
+	for (let i = 0; i < clicked.length; i++) {
+		// TODO: use clicked.forEach(elem => ...)
+		if (clicked[i].value != clicked[i].defaultValue) {
+			// TODO: use !== instead of !=
+			console.log("nie");
+			console.log(clicked[i].value);
 
-    
-    //remove user
-    const dismissalUser = (id) =>{
-        fetch(`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers/${id}`, {
-  method: 'DELETE'
-}).then(  
-    swal("successful", "user dismissaled", "success")    
-)
-    }
+			obj[clicked[i].id] = clicked[i].value;
+		} else {
+			console.log("tak");
+		}
+	}
 
-    //add class for editUserDetails
-    function wasClicked(e) {
-       e.classList.add('clicked')
-    }
+	if (id === undefined) {
+		// TODO: if (!id)
+		userId = {
+			id: Date.now()
+		};
+		resaul = {
+			// TODO: typo
+			...obj,
+			...userId
+		};
+		fetch(
+			"http://my-json-server.typicode.com/bisiakowsky/employer-list/employers", // TODO: adding/editing user could be extracted to separate function (example below)
+			{
+				method: "POST",
+				body: JSON.stringify(resaul),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8"
+				}
+			}
+		).then(
+			(document.querySelector(".employer-details").innerHTML = ""), // TODO: clearing html could be extracted to separate function
+			swal("successful", "user added", "success")
+		);
+	} else {
+		fetch(
+			`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers/${id}`,
+			{
+				method: "PUT",
+				body: JSON.stringify(obj),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8"
+				}
+			}
+		)
+			.then(res => res.json())
+			.then(json => console.log(json))
+			.then(swal("successful", "user updated", "success"));
+	}
+};
 
-    //check which input was changed
-    const editUserDetails = (id) =>{
-        
-        const clicked = document.getElementsByClassName('clicked')
-        let obj = {}
-
-        for (let i = 0; i < clicked.length; i++) {
-            if(clicked[i].value != clicked[i].defaultValue){
-                console.log('nie')
-                console.log(clicked[i].value)
-
-                obj[clicked[i].id] = clicked[i].value;
-
-            }else{
-                console.log('tak')
-            }
-                     
-        }
-        
-
-
-        if (id === undefined) {
-            userId = {
-                id: Date.now()
-            }
-            resaul = {
-                ...obj,
-                ...userId
-            }
-            fetch('http://my-json-server.typicode.com/bisiakowsky/employer-list/employers', {
-                method: 'POST',
-                body: JSON.stringify(resaul),
-                headers: {
-                  "Content-type": "application/json; charset=UTF-8"
-                }
-              })
-              .then(  
-                   document.querySelector('.employer-details').innerHTML = '',  
-                    swal("successful", "user added", "success")    
-                
-                   
-              )
-        }else{
-            fetch(`http://my-json-server.typicode.com/bisiakowsky/employer-list/employers/${id}`, {
-                method: 'PUT',
-                body: JSON.stringify(obj),
-                headers: {
-                  "Content-type": "application/json; charset=UTF-8"
-                }
-              }).then(  
-                swal("successful", "user updated", "success")    
-            )
-        } 
-    }
+// TODO: function sendUserData(user) {
+// TODO:   fetch(
+// TODO: 	 `http://my-json-server.typicode.com/bisiakowsky/employer-list/employers/${user.id}`,
+// TODO: 	 {
+// TODO: 		 method: `${user.id ? `PUT` : `POST`}`,
+// TODO: 		 body: JSON.stringify(user.data),
+// TODO: 		 headers: {
+// TODO: 			 "Content-type": "application/json; charset=UTF-8"
+// TODO: 		 }
+// TODO: 	 }
+// TODO:   ).then(swal("successful", `${user.id ? "user updated" : "user added"}`, "success"));
+// TODO: }
